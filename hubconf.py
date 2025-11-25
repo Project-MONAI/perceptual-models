dcd1d7f39d1866f6a963dc7ff2cdf28fdcc77ddd
Virginia Fernandez <61539159+virginiafdez@users.noreply.github.com>
2025-11-25 08:47:28 +0000

    Update import statements to use relative paths
    Signed-off-by: Virginia Fernandez <61539159+virginiafdez@users.noreply.github.com>

--- hubconf.py
+++ hubconf.py
@@ -1,12 +1,12 @@
 # Optional list of dependencies required by the package
 dependencies = ["torch"]
 
-from medicalnet.resnet import (  # noqa: E402
+from .medicalnet.resnet import (  # noqa: E402
     medicalnet_resnet10_23datasets,
     medicalnet_resnet50_23datasets,
 )
 
-from radimagenet.resnet import (  # noqa: E402
+from .radimagenet.resnet import (  # noqa: E402
     radimagenet_resnet50,
 )
 
